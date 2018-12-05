c8ad69051adf85f6236118d73196eef7900d18f8
leroy1260 <leroy1260@gmail.com>
2018-12-05 11:59:07 +0530

    Add column rename utility

--- src/main/scala/com/scienaptic/jobs/utility/Utils.scala
+++ src/main/scala/com/scienaptic/jobs/utility/Utils.scala
@@ -8,12 +8,17 @@ import scala.util.Try
 object Utils {
 
   def convertListToDFColumn(columnList: List[String], dataFrame: DataFrame) = {
-    columnList.map(name => dataFrame.col(name) /*.as(s"renamed_$name")*/)
+    columnList.map(name => dataFrame.col(name))
   }
 
   //TODO: Accept newname as List[String] and merge with 'name' to rename the column
-  def convertListToDFColumnWithRename(columnList: List[String], dataFrame: DataFrame, newName: String) = {
-    columnList.map(name => dataFrame.col(name).as(s"$newName"))
+  def convertListToDFColumnWithRename(renameMap: Map[String, String], dataFrame: DataFrame) = {
+    var newDF = dataFrame
+    for ((key,value) <- renameMap) yield newDF.withColumnRenamed(key, value)
+     /* renameMap.keySet.toList.foldLeft(dataFrame){ (df, col) =>
+      df.withColumnRenamed(col, renameMap.getOrElse(col, col))
+    }*/
+    newDF
   }
 
   def loadCSV(context: ExecutionContext, file: String): Try[DataFrame] = {
