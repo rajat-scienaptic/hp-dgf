e033dd20706aa085af7c42ad058fd5457f7f5805
PrashantN86 <PrashantN86@users.noreply.github.com>
2019-04-16 22:12:24 -0400

    updated NPC PC sample transformations

---
+++ src/main/scala/com/scienaptic/jobs/core/npd/print/transformations/Proc_Monthly_Update_FactTbl_NPDTech.scala
@@ -0,0 +1,54 @@
+package com.scienaptic.jobs.core.Print_jobs
+
+import com.scienaptic.jobs.ExecutionContext
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.StringType
+
+class Proc_Monthly_Update_FactTbl_NPDTech {
+
+  def execute(executionContext: ExecutionContext): Unit = {
+    val spark: SparkSession = executionContext.spark
+
+    val sourceMap = executionContext.configuration.sources
+
+    var stgtable =spark.sql("select * from ams_datamart_print.stgtable")
+
+    var Tbl_Master_NPD_Tech =spark.sql("select Sub_Category,Custom_Grouped_Max_Page_Width,NPD_Tech from ams_datamart_print.Tbl_Master_NPD_Tech")
+    Tbl_Master_NPD_Tech=Tbl_Master_NPD_Tech.withColumnRenamed("Sub_Category","SubCategory")
+      .withColumnRenamed("Custom_Grouped_Max_Page_Width","CustomGroupedMaxPageWidth")
+      .withColumnRenamed("NPD_Tech","NPDTech")
+
+
+    var TmpFctTable=stgtable.join(Tbl_Master_NPD_Tech,
+      stgtable("Sub_Category")===Tbl_Master_NPD_Tech("SubCategory") and
+        stgtable("Custom_Grouped_Max_Page_Width")===Tbl_Master_NPD_Tech("CustomGroupedMaxPageWidth")
+      ,"left")
+
+
+
+    TmpFctTable=TmpFctTable.withColumn("NPD_Tech",when(TmpFctTable("NPDTech").isNotNull,
+      TmpFctTable("NPDTech")).otherwise(lit(null).cast(StringType)))
+
+    TmpFctTable=TmpFctTable.drop("SubCategory","CustomGroupedMaxPageWidth","NPDTech")
+
+    //exportToHive(TmpFctTable,"","stgtable_temp","ams_datamart_print",executionContext)
+
+
+
+    val AMS_Commercial_Retail_udf=udf((value:String)=>{
+      value match {
+        case "Retail ALR" => "Retail"
+        case "Retail" => "Retail"
+        case "CA Retail" => "Retail"
+        case "CA Retail ALR" => "Retail"
+        case _=>"Commercial"
+      }
+    })
+
+    var TmpFctTable2=stgtable.withColumn("AMS_Commercial_Retail",AMS_Commercial_Retail_udf(stgtable("channel")))
+
+    //exportToHive(TmpFctTable2,"","stgtable_temp","ams_datamart_print",executionContext)
+
+  }
+}
--- src/main/scala/com/scienaptic/jobs/utility/NPDUtility.scala
+++ src/main/scala/com/scienaptic/jobs/utility/NPDUtility.scala
@@ -1,21 +1,54 @@
 package com.scienaptic.jobs.utility
 
-import java.util.Date
 import com.scienaptic.jobs.ExecutionContext
+import org.apache.log4j.Logger
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import com.scienaptic.jobs.core.npd.common.CommonTransformations._
 
 object NPDUtility {
 
-  def load_csv_to_table(executionContext: ExecutionContext,path:String,tablename:String):Unit={
+  val logger = Logger.getLogger(this.getClass.getName)
+
+  def load_csv_to_table(executionContext: ExecutionContext,path:String,datamart:String,tablename:String):Unit={
 
     val spark = executionContext.spark
-    var df=spark.read.option("escape","\"")
+    val df = spark.read.option("escape","\"")
       .option("inferSchema", true)
       .option("ignoreLeadingWhiteSpace","false").option("ignoreTrailingWhiteSpace","false")
       .option("header", "true").csv(path)
 
-    val esc="[ ,-]"
-    df=df.toDF(df.columns.map(x=>x.replaceAll(esc,"_")):_*)
-    df.write.mode(org.apache.spark.sql.SaveMode.Overwrite).saveAsTable("ams_datamart_print."+s"$tablename")
+    df
+      .transform(withCleanHeaders)
+      .write.mode(org.apache.spark.sql.SaveMode.Overwrite)
+      .saveAsTable(datamart+"."+tablename)
+
+  }
+
+  def writeToDataMart(spark: SparkSession,df : DataFrame,dataMart : String,tableName : String) = {
+
+    val sparkTableName = dataMart+"."+tableName+"_Spark"
+
+    df.createOrReplaceTempView(tableName)
+
+    spark.sql("drop table if exists "+sparkTableName)
+
+    spark.sql("create table "+sparkTableName+" STORED AS ORC AS select * from "+tableName)
+
+    logger.info("Creating hive table : "+sparkTableName)
+
+  }
+
+  def writeToDebugTable(spark: SparkSession,df : DataFrame,tableName : String) = {
+
+    val sparkTableName = "npd_sandbox.debug_"+tableName
+
+    df.createOrReplaceTempView(tableName)
+
+    spark.sql("drop table if exists "+sparkTableName)
+
+    spark.sql("create table "+sparkTableName+" STORED AS ORC AS select * from "+tableName)
+
+    logger.info("Creating hive table : "+sparkTableName)
 
   }
 
