bb294542a3c91e5e899862d5b8b3312bae1cde6e
PrashantN86 <PrashantN86@users.noreply.github.com>
2019-04-19 16:54:14 -0400

    added calender ams variables implementation for pc

--- src/main/scala/com/scienaptic/jobs/core/npd/common/CommonTransformations.scala
+++ src/main/scala/com/scienaptic/jobs/core/npd/common/CommonTransformations.scala
@@ -1,15 +1,49 @@
 package com.scienaptic.jobs.core.npd.common
 
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.{col, to_date, udf}
 
 object CommonTransformations {
 
+  /*
+  replaces spaces in column headers with "_" and drops round brackets from column names
+  */
   def withCleanHeaders(df :DataFrame)  = {
 
     df.toDF(df
       .schema
       .fieldNames
-      .map(name => "[ ,;{}.\\n\\t=]+".r.replaceAllIn(name, "_").toLowerCase): _*)
-
+      .map(name => {
+        "[ ,;{}.\\n\\t=]+".r.replaceAllIn(name, "_").toLowerCase
+          .replace("(","").replace(")","")
+      }): _*)
   }
+
+  /*
+  Updates converts time_periods to date to be utilised by other transformations
+  */
+  def timePeriodsToDate(df: DataFrame): DataFrame = {
+    df.withColumn("tmp_date",to_date(col("time_periods"), "MMM yyyy"))
+      .drop("time_periods").withColumnRenamed("tmp_date","time_periods")
+  }
+
+
+
+
+  def cleanDollars(df: DataFrame): DataFrame = {
+
+    val cleanUpDollers = (str : String) => {
+      str.replace("$","").replace(",","").toInt
+    }
+
+    def cleanDollersUDF = udf(cleanUpDollers)
+
+    df.withColumn("tmp_dollars",cleanDollersUDF(col("dollars")))
+      .drop("dollars").withColumnRenamed("tmp_dollars","dollars")
+
+    df
+  }
+
+
+
 }
--- src/main/scala/com/scienaptic/jobs/core/npd/pc/monthly/transformations/Transformer.scala
+++ src/main/scala/com/scienaptic/jobs/core/npd/pc/monthly/transformations/Transformer.scala
@@ -2,6 +2,7 @@ package com.scienaptic.jobs.core.npd.pc.monthly.transformations
 
 import com.scienaptic.jobs.ExecutionContext
 import com.scienaptic.jobs.core.npd.pc.monthly.transformations.MonthlyUSTransformations._
+import com.scienaptic.jobs.core.npd.common.CommonTransformations._
 
 object Transformer {
 
@@ -13,13 +14,14 @@ object Transformer {
     val DM_US_PC_Monthly_Dist_BTO_SOURCE = "Stg_DM_US_PC_Monthly_Dist_BTO"
     val DM_US_PC_Monthly_Retail_SOURCE = "Stg_DM_US_PC_Monthly_Retail"
 
-
     val spark = executionContext.spark;
     val df  = spark.sql("select * from "+DM_PC_US_Mth_Reseller_SOURCE)
 
     df
+      .transform(timePeriodsToDate)
+      .transform(cleanDollars)
+      .transform(withCalenderDetails)
       .transform(withASP)
-      //.transform(withASP)
       //.transform(withASP)
 
   }
