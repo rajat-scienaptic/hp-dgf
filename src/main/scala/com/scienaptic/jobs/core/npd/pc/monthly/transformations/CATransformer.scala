abaf00fbc63a61ce44b3ac2ccf8f77efc5ee223d
PrashantN86 <PrashantN86@users.noreply.github.com>
2019-04-28 08:49:41 -0400

    updated CA monthly pc

--- src/main/scala/com/scienaptic/jobs/core/npd/pc/monthly/transformations/CATransformer.scala
+++ src/main/scala/com/scienaptic/jobs/core/npd/pc/monthly/transformations/CATransformer.scala
@@ -1,9 +1,8 @@
 package com.scienaptic.jobs.core.npd.pc.monthly.transformations
 
 import com.scienaptic.jobs.ExecutionContext
-import com.scienaptic.jobs.core.npd.common.CommonTransformations._
-import com.scienaptic.jobs.core.npd.pc.monthly.transformations.CommonTransformations._
 import com.scienaptic.jobs.core.npd.pc.monthly.transformations.CATransformations._
+import com.scienaptic.jobs.core.npd.pc.monthly.transformations.CommonTransformations._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SaveMode}
 
@@ -12,27 +11,26 @@ object CATransformer {
   def withAllTransformations(df : DataFrame) = {
 
     val cleanUpDollers = (str : String) => {
-      str.replace("$","").replace(",","").toDouble
+      val dollar = str.replace("$","").replace(",","").toDouble
+      Math.round(dollar * 100.0) / 100.0
+    }
+
+    val cleanUpUnits = (str : String) => {
+      str.replace(",","").toInt
     }
 
     def cleanDollersUDF = udf(cleanUpDollers)
+    def cleanUnitsUDF = udf(cleanUpUnits)
 
     val finalDF = df
-      .withColumn("tmp_date",to_date(col("timeper"), "MMM yyyy"))
-      .drop("timeper")
-      .withColumnRenamed("tmp_date","time_periods")
+      .withColumn("tmp_date",to_date(col("timeper"), "MMM yyyy")).drop("timeper").withColumnRenamed("tmp_date","time_periods")
       .transform(withCalenderDetails)
-      .withColumn("tmp_dollars",
-        cleanDollersUDF(col("dollars")))
-      .drop("dollars")
-      .withColumnRenamed("tmp_dollars","dollars")
+      .withColumn("tmp_dollars", cleanDollersUDF(col("dollars"))).drop("dollars").withColumnRenamed("tmp_dollars","dollars")
+      .withColumn("tmp_units", cleanUnitsUDF(col("units"))).drop("units").withColumnRenamed("tmp_units","units")
       .transform(withExchangeRates)
       .transform(withCAASP)
-      //.transform(withCACategory)
       .withColumnRenamed("MODELA","MODEL")
       .transform(withSmartBuy)
-      //.transform(withCATopSellers)
-      //.transform(withCAPriceBand)
 
     finalDF
 
@@ -54,20 +52,24 @@ object CATransformer {
     val CAMthDist_int = CAMthDist_stg.transform(withAllTransformations)
     val CAMthRetail_int = CAMthRetail_stg.transform(withAllTransformations)
 
+    val historicalFact = spark.sql("select * from npd_sandbox.fct_tbl_ca_monthly_pc_historical")
+
     val cols1 = CAMthDist_int.columns.toSet
     val cols2 = CAMthRetail_int.columns.toSet
-    val total = cols1 ++ cols2 // union
+    val historic_columns = historicalFact.columns.toSet
 
-    def missingToNull(myCols: Set[String], allCols: Set[String]) = {
-      allCols.toList.map(x => x match {
+    val all_columns = historic_columns ++ cols1 ++ cols2 // union
+
+    def missingToNull(myCols: Set[String]) = {
+      all_columns.toList.map(x => x match {
         case x if myCols.contains(x) => col(x)
-        case _ => lit(null).as(x)
+        case _ => lit("NA").as(x)
       })
     }
 
-    val finalCAMonthlyDf =
-      CAMthDist_int.select(missingToNull(cols1,total):_*)
-      .union(CAMthRetail_int.select(missingToNull(cols2,total):_*))
+    historicalFact.select(missingToNull(historic_columns):_*)
+      CAMthDist_int.select(missingToNull(cols1):_*)
+      .union(CAMthRetail_int.select(missingToNull(cols2):_*))
       .write.mode(SaveMode.Overwrite)
       .saveAsTable(DATAMART+"."+TABLE_NAME);
 
